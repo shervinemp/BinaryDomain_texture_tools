cb79d9923ab4de315a46d3dbe56602d1c386bf84
shervin_emperor <shervin.naseri@gmail.com>
2024-06-07 01:21:10 -0400

    fix errors due to restructuring

--- update_files.py
+++ update_files.py
@@ -4,10 +4,10 @@ import subprocess
 
 from utils import get_par_dirs, run_proc
 
-PAR_TOOL = os.path.join(os.path.dirname(__file__), "ParTool.exe")
+PAR_TOOL = "ParTool.exe"
 PAR_TOOL_ARGS = "add -c 1"
 MODIFIED_DIR = os.path.abspath("__modified")
-TEMP_DIR = os.path.abspath("__tmp__")
+TEMP_DIR = os.path.abspath(".tmp")
 BACKUP_DIR = os.path.abspath("__backup")
 
 
@@ -25,18 +25,25 @@ if __name__ == "__main__":
         c = input(
             f"The contents of {TEMP_DIR} must be cleared to avoid conflict. Would you like to proceed? (y/n) "
         )
-        if c.lower() != "y":
+        if c.lower() == "y":
             shutil.rmtree(TEMP_DIR)
         else:
             exit()
 
     print(f'Copying files to "{TEMP_DIR}" for processing...')
-    shutil.copytree(MODIFIED_DIR, TEMP_DIR, dirs_exist_ok=True)
+    shutil.copytree(MODIFIED_DIR, TEMP_DIR)
 
     for dir_path in get_par_dirs(TEMP_DIR):
         dir_relpath = os.path.relpath(dir_path, TEMP_DIR)
-        # remove the leading underscore of the directory name
-        file_relpath = os.path.join((s := os.path.split(dir_relpath))[0], s[1][1:])
+
+        # remove the leading underscore of the directory name and also the tag directories.
+        s_ = os.path.split(dir_relpath)
+        r_ = s_[0].split(os.sep)
+        file_relpath = os.path.join(
+            r_[0],
+            *r_[2:],
+            s_[1][1:],
+        )
 
         dest_path = os.path.join(TEMP_DIR, file_relpath)
         backup_path = os.path.join(BACKUP_DIR, file_relpath)
@@ -52,6 +59,11 @@ if __name__ == "__main__":
             )
 
             backup(file_relpath)
+
+            # remove destpath if it exists
+            if os.path.exists(dest_path):
+                os.remove(dest_path)
+
             run_proc(command_string)
         except ValueError as e:
             print(str(e))
