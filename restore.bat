be815af021dfc3c068eb9c28deb71c9276ba3450
shervin_emperor <shervin.naseri@gmail.com>
2024-06-07 06:22:08 -0400

    simplify script and fix

--- restore.bat
+++ restore.bat
@@ -1,40 +1,23 @@
 @echo off
 setlocal enabledelayedexpansion enableextensions
 
-call :full_path backup_dir "backup_"
+call :full_path backup_dir "__backup"
 
 set /p "confirm=Do you want to restore backups? (Y/N) "
 
 if /i "%confirm%"=="y" (
-    set /p "confirm_all=Do you want to restore all (Y) or one at a time (N)? "
-    set "confirm_one=y"
-
     if exist "%backup_dir%" (
         for /r "%backup_dir%" %%d in (*.par) do (
             set "backup_path=%%~fd"
             set "backup_name=%%~nd"
 
-            if /i "%confirm_all%"=="n" (
-                set /p "confirm_one=Do you want to restore !backup_name!? (Y/N) "
-            )
+            set "file_path=!backup_path:%backup_dir%=%cd%!"
 
-            if /i "%confirm_one%"=="y" (
-                set "file_path=!backup_path:%backup_dir%=%cd%!"
-
-                copy /y "%%d" "!file_path!"
-                echo "Restored !file_path!"
-            )
+            copy /y "%%d" "!file_path!"
+            echo "Restored !file_path!"
         )
 
         echo "Backup files restored to current directory."
-
-        if /i "%confirm_all%"=="y" (
-            set /p "confirm_delete=Do you want to delete the backup directory? (Y/N) "
-        )
-        if /i "%confirm_delete%"=="y" (
-            rd /s /q "%backup_dir%"
-            echo "Deleted backup directory %backup_dir%."
-        )
     )
 )
 goto :eof
