4319746313197c3a53e2b82f0ea9a0d69740709f
shervin_emperor <shervin.naseri@gmail.com>
2024-06-10 15:06:45 -0400

    remove dunder directories from being removed

--- clean.bat
+++ clean.bat
@@ -1,24 +1,24 @@
 @echo off
 setlocal enabledelayedexpansion enableextensions
 
-call :full_path staged_dir "__staged"
-call :full_path unstaged_dir "__unstaged"
-
 call "%~dp0\restore.bat"
 
 set /p "confirm=Do you want to remove the extracted directories? (Y/N) "
 
 if /i "%confirm%"=="y" (
+    
     for /d /r "%cd%" %%G in (*.par) do (
-        if /i not "%%~fG"=="%cd%\%staged_dir%" (
-            if /i not "%%~fG"=="%cd%\%unstaged_dir%" (
-                echo Removing directory "%%G" ...
-                rd "%%G" /s /q 2>nul
-            )
+        @REM expand remove %cd% from path
+        set "full_path=%%~fG"
+        set "relative_path=!full_path:%cd%\=!"
+        if not "!relative_path:~0,2!" == "__" (
+            echo Removing directory "!relative_path!"...
+            rd "%%G" /s /q 2>nul
         )
     )
 
     echo All directories with suffix ".par" have been removed.
+
 ) else (
     echo Operation canceled by user.
 )
