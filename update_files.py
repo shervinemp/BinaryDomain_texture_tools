c12f3cbf8094b44de6393ccf3ecc609d09657dcf
shervin_emperor <shervin.naseri@gmail.com>
2024-06-16 02:51:06 -0400

    refactor

--- update_files.py
+++ update_files.py
@@ -79,6 +79,45 @@ class FileHashLedger:
         self._save()
 
 
+def update_par(
+    source_dir: str,
+    target_path: str,
+    ledger: FileHashLedger,
+    backup_as_source: bool = False,
+) -> None:
+    file_relpath = os.path.relpath(target_path, os.getcwd())
+    backup_path = os.path.join(BACKUP_DIR, file_relpath)
+    dest_path = os.path.join(TEMP_DIR, file_relpath)
+
+    if not os.path.isfile(backup_path):
+        os.makedirs(os.path.dirname(backup_path), exist_ok=True)
+        shutil.copy2(file_relpath, backup_path)
+
+    source_path = backup_path if backup_as_source else file_relpath
+    if not os.path.exists(file_relpath):
+        print(f'File "{file_relpath}" could not be found. Using backup as source.')
+        source_path = backup_path
+        ledger.clear(source_dir)
+
+    diff = ledger.get_diff(source_dir)
+    if len(diff) == 0:
+        link_compat(source_path, dest_path)
+        shutil.rmtree(source_dir)
+        print("No changes detected. Skipping update...")
+        return
+
+    command_string = (
+        f'{PAR_TOOL} {PAR_TOOL_ARGS} "{source_path}" "{source_dir}" "{dest_path}"'
+    )
+    run_proc(command_string)
+    shutil.rmtree(source_dir)
+
+    if os.path.exists(file_relpath):
+        os.remove(file_relpath)
+    link_compat(dest_path, file_relpath)
+    ledger.push(diff)
+
+
 def main():
     args = parser.parse_args()
 
@@ -102,45 +141,17 @@ def main():
 
         for dir_path in get_par_dirs(TEMP_DIR):
             dir_relpath = os.path.relpath(dir_path, TEMP_DIR)
-            # remove the leading underscore of the directory name.
-            file_relpath = os.path.join(
+            target_path = os.path.join(
                 (s_ := os.path.split(dir_relpath))[0], s_[1][1:]
-            )
-
-            backup_path = os.path.join(BACKUP_DIR, file_relpath)
-            dest_path = os.path.join(TEMP_DIR, file_relpath)
+            )  # remove the leading underscore of the directory name.
 
             print(f'Processing "{dir_relpath}"...')
-
-            if not os.path.isfile(backup_path):
-                os.makedirs(os.path.dirname(backup_path), exist_ok=True)
-                shutil.copy2(file_relpath, backup_path)
-
-            source_path = backup_path if args.fresh else file_relpath
-            if not os.path.exists(file_relpath):
-                print(
-                    f"File {file_relpath} could not be found. Using backup as source."
-                )
-                source_path = backup_path
-                ledger.clear(dir_path)
-
-            diff = ledger.get_diff(dir_path)
-            if len(diff) == 0:
-                link_compat(source_path, dest_path)
-                shutil.rmtree(dir_path)
-                print("No changes detected. Skipping update...")
-                continue
-
-            command_string = (
-                f'{PAR_TOOL} {PAR_TOOL_ARGS} "{source_path}" "{dir_path}" "{dest_path}"'
+            update_par(
+                source_dir=dir_path,
+                target_path=target_path,
+                ledger=ledger,
+                backup_as_source=args.fresh,
             )
-            run_proc(command_string)
-            shutil.rmtree(dir_path)
-
-            if os.path.exists(file_relpath):
-                os.remove(file_relpath)
-            link_compat(dest_path, file_relpath)
-            ledger.push(diff)
 
     finally:
         shutil.rmtree(TEMP_DIR)
